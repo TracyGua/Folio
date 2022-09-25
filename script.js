fa66381ab1e7de9d237071c82d9d0928c6f129f6
TracyGua <tgua391@gmail.com>
2022-09-25 19:23:02 -0400

    animation accueil

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const floatAnimation = () => {
-  const nom = new TimelineMax({ repeat: -1 });
+  const tlCan = new TimelineMax({ repeat: -1 });
   /*Can Animation*/
-  nom
+  tlCan
     //move top left
-    .to(".nom-evenement", 3, {
+    .to(".can", 3, {
       y: "-=30",
       x: "+=20",
       rotation: "-=5",
@@ -11,29 +11,31 @@ const floatAnimation = () => {
     })
 
     //move down right
-    .to(".nom-evenement", 2, {
+    .to(".can", 2, {
       y: "+=30",
       x: "-=20",
       rotation: "-=5",
       ease: Power1.easeInOut,
     })
 
-    .to(".can > img", 3, { y: "-=20", rotation: "+=5", ease: Power1.easeInOut })
+    .to(".can", 3, { y: "-=20", rotation: "+=5", ease: Power1.easeInOut })
 
-    .to(".can > img", 3, { y: "+=20", rotation: "+=5", ease: Power1.easeInOut })
+    .to(".can", 3, { y: "+=20", rotation: "+=5", ease: Power1.easeInOut })
 
-    .to(".can > imgn", 3, { y: "-=50", ease: Power1.easeInOut })
+    .to(".can", 3, { y: "-=50", ease: Power1.easeInOut })
 
-    .to(".can > img", 3, { y: "+=50", ease: Power1.easeInOut })
+    .to(".can", 3, { y: "+=50", ease: Power1.easeInOut })
 
-    .to(".can > img", 3, { y: "-=30", ease: Power1.easeInOut })
+    .to(".can", 3, { y: "-=30", ease: Power1.easeInOut })
 
-    .to(".can > img", 3, { y: "+=30", ease: Power1.easeInOut })
+    .to(".can", 3, { y: "+=30", ease: Power1.easeInOut })
 
-    .to(".can > img", 2, { y: "-=30", ease: Power1.easeInOut })
+    .to(".can", 2, { y: "-=30", ease: Power1.easeInOut })
 
-    .to(".can > img", 2, { y: "+=30", ease: Power1.easeInOut });
+    .to(".can", 2, { y: "+=30", ease: Power1.easeInOut });
 
-  TweenLite.to(nom, 27, { ease: Power1.easeInOut });
+  TweenLite.to(tlCan, 27, { ease: Power1.easeInOut });
 };
 floatAnimation();
+
+//navigation
